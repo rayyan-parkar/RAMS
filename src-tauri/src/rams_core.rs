ef0dd98342087c4a207a3f8a7dc242c451831128
Rayyan Parkar <rayyanparkar09@gmail.com>
2026-04-25 22:37:57 +0100

    Updated Cargo.toml to allocate the future framework structure

--- src-tauri/src/rams_core.rs
+++ src-tauri/src/rams_core.rs
@@ -1,26 +1,46 @@
 use std::time::Instant;
 use str0m::media::{Direction, MediaKind};
 use str0m::Rtc;
-use str0m::RtcConfig;
+use tauri::Url;
+use std::net::TcpListener;
 
 struct Rams{
     rtc_client : Rtc,
+    state: RamsConnection
 }
 
 enum RamsError {
-    IceFailure
+    IceFailure,
+    PortFailure(String),
 }
 
+pub enum RamsConnection {
+    New,
+    LocalOfferCreated,
+    WaitingForAnswer,
+    Connecting,
+    Connected,
+    Failed
+}
+// You look quite wonderful today
 impl Rams {
-    pub fn quick_connect(mut self) ->Result<Rams, RamsError> {
-
-        let mut rtc = Rtc::new(Instant::now());
-        let mut changes = rtc.sdp_api();
-
-        let audio = changes.add_media(MediaKind::Audio, Direction::SendOnly, None, None, None);
-        let video = changes.add_media(MediaKind::Video, Direction::SendOnly, None, None, None);
+    pub fn new() ->  Self {
+        let client = Rtc::new(Instant::now());
+        Self {
+            rtc_client : client,
+            state : RamsConnection::New
+        }
+    }
+    pub fn quick_connect(&mut self, connect_to : Url) ->Result<&mut Self, RamsError> {
+        let mut changes = self.rtc_client.sdp_api();
+        let mid_audio = changes.add_media(MediaKind::Audio, Direction::SendOnly, None, None, None);
+        let mid_video = changes.add_media(MediaKind::Video, Direction::SendOnly, None, None, None);
 
         let (offer, pending) = changes.apply().unwrap();
+        self.state = RamsConnection::WaitingForAnswer;
+
+        // Make a listener to listen on a random port decided by the OS
+        let _listener : TcpListener = TcpListener::bind("127.0.0.1:0").map_err( |e| RamsError::PortFailure(e.to_string()))?;
 
         Ok(self)
     }
