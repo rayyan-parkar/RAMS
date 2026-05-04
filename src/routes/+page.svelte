0ce3e1bdf00235695bcee6068f3b10b8b6e8c778
Rayyan Parkar <rayyanparkar09@gmail.com>
2026-05-04 23:47:20 +0100

    App is finally done :D

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,8 @@
   let localStream: MediaStream | null = null;
 
   // Fullscreen mode
-  let isFullscreen = $state(false);
+  let fullscreenTarget = $state<'local' | 'remote' | null>(null);
+  let isFullscreen = $derived(fullscreenTarget !== null);
   let showExitButton = $state(false);
   let exitButtonTimeout: ReturnType<typeof setTimeout> | null = null;
   let fullscreenContainer: HTMLDivElement | null = null;
@@ -35,10 +36,14 @@
 
   // Robust fullscreen logic using pure CSS class toggling
   // Bypasses native requestFullscreen() which causes transient activation & RangeBuffer errors in WebKitGTK
-  function toggleFullscreen() {
-    isFullscreen = !isFullscreen;
+  function toggleFullscreen(target: 'local' | 'remote' | null) {
+    if (target === null || fullscreenTarget === target) {
+      fullscreenTarget = null;
+    } else {
+      fullscreenTarget = target;
+    }
     if (exitButtonTimeout) clearTimeout(exitButtonTimeout);
-    if (!isFullscreen) {
+    if (!fullscreenTarget) {
       showExitButton = false;
     }
   }
@@ -984,6 +989,8 @@
 <div
   class="container"
   class:fullscreen-mode={isFullscreen}
+  class:target-local={fullscreenTarget === 'local'}
+  class:target-remote={fullscreenTarget === 'remote'}
   bind:this={fullscreenContainer}
   onmousemove={handleMouseMove}
   role="main"
@@ -1009,14 +1016,14 @@
           <span>> LOCAL_TX [H.264]</span>
           <video bind:this={localVideoRef} autoplay muted playsinline class="glow"></video>
           {#if connectionState === 'CONNECTED'}
-            {#if isFullscreen}
+            {#if fullscreenTarget === 'local'}
               {#if showExitButton}
-                <button class="exit-fullscreen-btn icon-btn" title="Exit Fullscreen" onclick={toggleFullscreen}>
+                <button class="exit-fullscreen-btn icon-btn" title="Exit Fullscreen" onclick={() => toggleFullscreen(null)}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8 3v3a2 2 0 0 1-2 2H3"/><path d="M21 8h-3a2 2 0 0 1-2-2V3"/><path d="M3 16h3a2 2 0 0 1 2 2v3"/><path d="M16 21v-3a2 2 0 0 1 2-2h3"/></svg>
                 </button>
               {/if}
-            {:else}
-              <button class="fullscreen-btn icon-btn" title="Fullscreen" onclick={toggleFullscreen}>
+            {:else if !isFullscreen}
+              <button class="fullscreen-btn icon-btn" title="Fullscreen" onclick={() => toggleFullscreen('local')}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8 3H5a2 2 0 0 0-2 2v3"/><path d="M21 8V5a2 2 0 0 0-2-2h-3"/><path d="M3 16v3a2 2 0 0 0 2 2h3"/><path d="M16 21h3a2 2 0 0 0 2-2v-3"/></svg>
               </button>
             {/if}
@@ -1025,6 +1032,19 @@
         <div class="video-box remote">
           <span>> REMOTE_RX [H.264]</span>
           <video bind:this={remoteVideoRef} autoplay playsinline></video>
+          {#if connectionState === 'CONNECTED'}
+            {#if fullscreenTarget === 'remote'}
+              {#if showExitButton}
+                <button class="exit-fullscreen-btn icon-btn" title="Exit Fullscreen" onclick={() => toggleFullscreen(null)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8 3v3a2 2 0 0 1-2 2H3"/><path d="M21 8h-3a2 2 0 0 1-2-2V3"/><path d="M3 16h3a2 2 0 0 1 2 2v3"/><path d="M16 21v-3a2 2 0 0 1 2-2h3"/></svg>
+                </button>
+              {/if}
+            {:else if !isFullscreen}
+              <button class="fullscreen-btn icon-btn" title="Fullscreen" onclick={() => toggleFullscreen('remote')}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8 3H5a2 2 0 0 0-2 2v3"/><path d="M21 8V5a2 2 0 0 0-2-2h-3"/><path d="M3 16v3a2 2 0 0 0 2 2h3"/><path d="M16 21h3a2 2 0 0 0 2-2v-3"/></svg>
+              </button>
+            {/if}
+          {/if}
         </div>
       </div>
       <div class="media-controls mt-4">
